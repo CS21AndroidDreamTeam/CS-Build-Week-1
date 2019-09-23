1d5a59e273b85d805ebaeb2a899e876341efbdf2
Jake Esveld <thejakeesveld@gmail.com>
2019-09-23 15:40:18 -0700

    Create basic models (No relational values)

--- api/models.py
+++ api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=200)
+    desc = models.TextField(max_length=1000)
+    positionx = models.IntegerField()
+    positiony = models.IntegerField()
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=200)
+    positionx = models.IntegerField()
+    positiony = models.IntegerField()
+
+class Item(models.Model):
+    name = models.CharField(max_length=200)
+    desc = models.TextField(max_length=1000)
