c0dc0e990fe84606aa3751629e0bc47cad4e8a48
Jake Esveld <thejakeesveld@gmail.com>
2019-09-24 10:19:10 -0700

    Set up basic Procedural Gen

--- util/sample_generator.py
+++ util/sample_generator.py
@@ -4,37 +4,32 @@
 # You can modify generate_rooms() to create your own
 # procedural generation algorithm and use print_rooms()
 # to see the world.
+import random
+from api.models import Player, Room, Item
 
 
-class Room:
-    def __init__(self, id, name, description, x, y):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.n_to = None
-        self.s_to = None
-        self.e_to = None
-        self.w_to = None
-        self.x = x
-        self.y = y
-    def __repr__(self):
-        if self.e_to is not None:
-            return f"({self.x}, {self.y}) -> ({self.e_to.x}, {self.e_to.y})"
-        return f"({self.x}, {self.y})"
-    def connect_rooms(self, connecting_room, direction):
-        '''
-        Connect two rooms in the given n/s/e/w direction
-        '''
-        reverse_dirs = {"n": "s", "s": "n", "e": "w", "w": "e"}
-        reverse_dir = reverse_dirs[direction]
-        setattr(self, f"{direction}_to", connecting_room)
-        setattr(connecting_room, f"{reverse_dir}_to", self)
-    def get_room_in_direction(self, direction):
-        '''
-        Connect two rooms in the given n/s/e/w direction
-        '''
-        return getattr(self, f"{direction}_to")
 
+def gen_room(self, ranInt, roomId):
+    room_names = ["Prehistoric Cave", "Medieval Wizards Tower", "Abandoned Cottage", "Alchemists Laboratory", "Viking Barracks",
+                    "Roman Colloseseum", "London Shop", "Futuristic Lab", "Barbaric Outpost", "Western Town", "Colonial Puritan Church",
+                    "Nazi Meeting Hall", "Chinese Pagoda", "Cold War Nuclear Site", "Beached Pirate Ship"]
+    room_descriptions = ["This seems to be one of the first shelters ever to be used by mankind. A special sort of novelty. Also, it seems pretty empty...",
+                         "A magical place! This is the type of place you hear about in a story book! I never knew that they were real, once upon a time....",
+                         "This cottage seems to have been left long ago. The place is a bit worse for ware. I wonder who used to live here?",
+                         "Ah, this is a place dedicated to one thing, THE SEARCH FOR GOLD! But not in the conventional way. It doesn't seem like they ever had much luck.",
+                         "A rough and run down place. You see weapons of all sorts lying around, and what appears to be a half eaten turkey leg...",
+                         "The birthplace of entertainment! People are all around, and you can faintly hear the roar of the lion in its cage.",
+                         "A daily visit for most in this cobblestone village. Goods from all around, and some say this is the best place for gossip.",
+                         "Everything around you is either chrome or the brightest white you've ever seen. This is the epitome of a sterile environment.",
+                         "The people around here look like they're in a \"Homeless Cave Man\" costume contest. They smell like it too...",
+                         "Ah, the Wild West! Just how the movies always depicted it. A real life western saloon complete with a spitoon on the deck!",
+                         "The people around here look nice enough, but you'd best stand up straight and remember your please and thank you's",
+                         "You never thought you'd be anywhere on this side of the war. This place looks fine enough, it's more the people you are worried about.",
+                         "This place is the epitome of the far east. From the finial at the top, to the feng-shui garden on the ground below.",
+                         "This place is oozing with secrecy. You can feel the tension in the air. No one seems to be very friendly of outsiders here...",
+                         "ARGH ME MATEY! A real life pirate ship! I wonder if there is any \"booty\" on board?"]
+    room = Room(roomId, room_names[ranInt], room_descriptions[ranInt])
+    return room
 
 class World:
     def __init__(self):
@@ -47,52 +42,39 @@ class World:
         '''
 
         # Initialize the grid
-        self.grid = [None] * size_y
+        self.grid = [None] * size_x
         self.width = size_x
         self.height = size_y
         for i in range( len(self.grid) ):
-            self.grid[i] = [None] * size_x
+            self.grid[i] = [None] * size_y
 
-        # Start from lower-left corner (0,0)
-        x = -1 # (this will become 0 on the first step)
-        y = 0
-        room_count = 0
+        idCount = 0
+        for x in range(size_x):
+            for y in range(size_y):
+                ranInt = random.randint(0,9)
+                if ranInt < 6:
+                    room = Room(idCount, "Road", "Just a dusty old road")
+                    idCount += 1
+                    self.grid[x][y] = room
+                    room.save()
+                else:
+                    ran = random.randint(0,14)
+                    room = gen_room(ran, idCount)
+                    idCount += 1
+                    self.grid[x][y] = room
+                    room.save()
 
-        # Start generating rooms to the east
-        direction = 1  # 1: east, -1: west
 
-
-        # While there are rooms to be created...
-        previous_room = None
-        while room_count < num_rooms:
-
-            # Calculate the direction of the room to be created
-            if direction > 0 and x < size_x - 1:
-                room_direction = "e"
-                x += 1
-            elif direction < 0 and x > 0:
-                room_direction = "w"
-                x -= 1
-            else:
-                # If we hit a wall, turn north and reverse direction
-                room_direction = "n"
-                y += 1
-                direction *= -1
 
             # Create a room in the given direction
-            room = Room(room_count, "A Generic Room", "This is a generic room.", x, y)
+            #room = Room(room_count, "A Generic Room", "This is a generic room.", x, y)
             # Note that in Django, you'll need to save the room after you create it
 
             # Save the room in the World grid
             self.grid[y][x] = room
 
-            # Connect the new room to the previous room
-            if previous_room is not None:
-                previous_room.connect_rooms(room, room_direction)
+            #room_count += 1
 
-            # Update iteration variables
-            previous_room = room
-            room_count += 1
 
 
 
